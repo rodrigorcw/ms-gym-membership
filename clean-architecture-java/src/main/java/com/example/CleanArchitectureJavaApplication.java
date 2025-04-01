810335458e39738c3bb3a426b52560b91a2edae9
rodrigo wenzel <rodrigocw90@gmail.com>
2025-04-01 13:51:30 -0300

    Initial commit

---
+++ clean-architecture-java/src/main/java/com/example/CleanArchitectureJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanArchitectureJavaApplication.class, args);
+	}
+
+}
